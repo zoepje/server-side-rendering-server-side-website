29c0fec91d151e385d460137c7f97bb1656e017b
zoepje <zoe.penner@hva.nl>
2024-03-05 12:30:07 +0100

    server opstarten

---
+++ server.js
@@ -0,0 +1,31 @@
+/*** Express setup & start ***/
+
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from './helpers/fetch-json.js'
+
+// Importeer het npm pakket express uit de node_modules map
+import express, { response } from 'express'
+
+// Maak een nieuwe express app aan
+const app = express()
+
+// Stel ejs in als template engine
+app.set('view engine', 'ejs')
+
+// Stel de map met ejs templates in
+app.set('views', './views')
+
+// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+app.use(express.static('public'))
+
+// Zorgt dat werken met request data makkelijker wordt
+app.use(express.urlencoded({extended: true}))
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set('port', process.env.PORT || 8000)
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
