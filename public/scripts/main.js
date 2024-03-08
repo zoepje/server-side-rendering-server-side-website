51568adda6a477f188952c41bb773f27e8346bbd
zoepje <zoe.penner@hva.nl>
2024-03-08 10:35:02 +0100

    edit

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,5 @@
-let initialDate = document.querySelector('.date')
+// let initialDate = document.querySelector('.date')
+let initialDate = '2024-12-30 19:00:00'
 
 let formattedDate = new Date(initialDate).toLocaleDateString('nl-NL', {
   day: '2-digit',
