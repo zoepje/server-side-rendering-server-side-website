a31ca24bd09cad4ee47b8495ff975b5f2d5a4367
zoepje <zoe.penner@hva.nl>
2024-03-12 15:59:25 +0100

    uitklap menu

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,25 @@
 // let initialDate = document.querySelector('.date')
-let initialDate = '2024-12-30 19:00:00'
+// let initialDate = '2024-12-30 19:00:00'
 
 // string replace 
-let formattedDate = new Date(initialDate).toLocaleDateString('nl-NL', {
-  day: '2-digit',
-  month: 'short',
-  hour12: false
+// let formattedDate = new Date(initialDate).toLocaleDateString('nl-NL', {
+//   day: '2-digit',
+//   month: 'short',
+//   hour12: false
 
-})
-console.log(formattedDate)
+// })
+// console.log(formattedDate)
+
+
+const menuBtn = document.querySelector('.menu-button'),
+      // closeBtn = document.querySelector('.close-button'),
+      nav = document.querySelector('nav'),
+      header = document.querySelector('header');
+
+// Functie uitklappen menu
+function toggleMenu(){
+  header.classList.toggle("nav-open");
+}
+
+// als je klikt op dit element dan voer deze funtie uit.
+menuBtn.addEventListener("click", toggleMenu);
